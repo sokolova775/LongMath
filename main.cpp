f10a6ff3b96e6591df55c174f4f2936075d30844
Alkisokolova@edu.hse.ru <timofejbulgakov@192.168.1.4>
2024-02-18 00:38:39 +0300

    tests and bug fixes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include "LongNumber.h"
+#include"LongNumber/LongNumber.h"
 
 int main() {
     auto a = LongMath::LongNumber(143);
     auto d = LongMath::LongNumber(12);
-    auto b = LongMath::LongNumber("-2345");
+    auto b = LongMath::LongNumber("-100000000");
     auto c = LongMath::LongNumber("12345.5126456125");
     std::cout << b << std::endl;
-    std::cout << (c/b) << std::endl;
+     std::cout << (c/b) << std::endl;
     std::cout << c << std::endl;
     return 0;
 }
