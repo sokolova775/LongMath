efa546f19396fbed11ce448d24d7b59b385235e6
sokolova775 <157019870+sokolova775@users.noreply.github.com>
2024-02-22 12:20:31 +0300

    Update main.cpp
    final 2.0

--- main.cpp
+++ main.cpp
@@ -4,13 +4,15 @@
 
 int main() {
     int  n;
-    std::cout << "введите точность: ";
-    std::cin >> n;
     clock_t start, finish;
+
+
+    std::cout << "introduce precision: ";
+    std::cin >> n;
     start = clock();
     auto _pi = PI::pi(n);
     finish = clock();
-    std::cout << "pi: " << _pi.toString(n)<< std::endl;
-    std::cout << "time: " <<static_cast<double>(finish - start)/CLOCKS_PER_SEC << std::endl;
+    std::cout << "pi = " << _pi.toString(n) << std::endl;
+    std::cout << "time: " << static_cast<double>(finish - start)/CLOCKS_PER_SEC << " sec" << std::endl;
     return 0;
 }
